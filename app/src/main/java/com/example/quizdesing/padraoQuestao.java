2e4fb9f5a42f4f0d2ad5fe2ae7b29e3a44cdd926
iris_celestino@hotmail.com <iris2991176@aluno.ba.senac.br>
2023-09-01 09:51:31 -0300

    primeiro commit

---
+++ app/src/main/java/com/example/quizdesing/padraoQuestao.java
@@ -0,0 +1,14 @@
+package com.example.quizdesing;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class padraoQuestao extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_padrao_questao);
+    }
+}
---
+++ app/src/main/java/com/example/quizdesing/telaPrincipal.java
@@ -0,0 +1,19 @@
+package com.example.quizdesing;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class telaPrincipal extends AppCompatActivity {
+
+
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tela_principal);
+    }
+}
